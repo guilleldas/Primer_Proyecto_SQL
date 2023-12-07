c1ef10a40ea264c23f341b3320a58f27f79a132e
Guillermo Lodeiro <145810000+guilleldas@users.noreply.github.com>
2023-12-07 18:02:41 +0100

    Sigo formulando queries.

--- Queries.sql
+++ Queries.sql
@@ -1,46 +1,43 @@
-# TABLA: PRODUCTLINES + PRODUCTS
+# Las siguientes queries fueron pensadas para extraer información valiosa e interesante del dataset.
 
-#Para seguir entendiendo la BBDD creo que es bueno saber cuales son las lineas de productos
-# y que cantidad de modelos existen dentro de cada una de ellas. Con la siguiente query podemos ver que 
-# las lineas son Classic Cars, Vintage Cars, Motorcycles, Planes, Trucks and Buses, Ships y Trains.
-# Siendo Classic Cars la línea con más modelos y Trains la línea con menos modelos. Esto no significa que
-# Classic Cars sea la más vendida sino que simplemente tiene más modelos. 
+TABLA: PRODUCTLINES
 
-# MÁS ADELANTE SE PUDE ANALIZAR SI EXISTE UNA RELACION ENTRE LA CANTIDAD DE MODELOS DE CADA LINEA 
-# Y LAS VENTAS.
+Comenzando por la tabla Productlines y creo que es bueno saber cuales son las lineas de productos y que cantidad de modelos existen dentro de cada una de ellas. 
+Con la siguiente query podemos ver que las lineas son Classic Cars, Vintage Cars, Motorcycles, Planes, Trucks and Buses, Ships y Trains. 
+Siendo Classic Cars la línea con más modelos y Trains la línea con menos modelos. Esto no significa que Classic Cars sea la más vendida sino que simplemente tiene más modelos. 
 
-/*SELECT pl.productline AS ProductLine, COUNT(pr.productCode) AS Model_Qtty
+SELECT productline AS ProductLine, COUNT(productCode) AS Model_Qtty
+FROM products
+GROUP BY productline
+ORDER BY COUNT(productCode) DESC
+
+En cuanto al stock, podemos obtener el stock existente por linea de productos y cual es su porcentaje sobre el stock total. En este caso Classic cars tiene un stock considerablemente mayor
+al de las demás líneas, por otro lado Trains es el que menor stock maneja. 
+  
+SELECT productline AS ProductLine, SUM(quantityinstock) AS Stock, 
+ROUND((SUM(quantityinstock) / (SELECT SUM(quantityinstock) FROM products)) * 100,1) AS StockPercentage
+FROM products
+GROUP BY productline
+ORDER BY SUM(quantityinstock) DESC
+
+#(Haciendo un paréntesis y para demostrar el uso de JOIN, en caso de que Productline no fuere clave foránea en products, deberíamos haber hecho las siguientes consultas para obtener el mismo resultado
+#que en las queries de arriba.
+  
+SELECT pl.productline AS ProductLine, COUNT(pr.productCode) AS Model_Qtty
 FROM productlines as pl
 JOIN products as pr ON pr.productline = pl.productline
 GROUP BY productline
-ORDER BY COUNT(pr.productCode) DESC*/
+ORDER BY COUNT(pr.productCode) DESC
 
-## SIN NECESIDAD DE JOIN!!!! OJOOOO
-
-/*SELECT productline AS ProductLine, COUNT(productCode) AS Model_Qtty
-FROM products
-GROUP BY productline
-ORDER BY COUNT(productCode) DESC*/
-
-#Nuevamente observando las tablas Productlines y Product podemos obtener el stock que existe por
-#linea de productos y cual es su porcentaje sobre el total del stock. 
-#En este caso Classic cars tiene un stock considerablemente más grande al de 
-#las demás líneas, por otro lado Trains es el que menor stock maneja. 
-
-/*SELECT pl.productline AS ProductLine, SUM(pr.quantityinstock) AS Stock, 
+SELECT pl.productline AS ProductLine, SUM(pr.quantityinstock) AS Stock, 
 ROUND((SUM(pr.quantityinstock) / (SELECT SUM(quantityinstock) FROM products)) * 100,1) AS StockPercentage
 FROM productlines as pl
 JOIN products as pr ON pr.productline = pl.productline
 GROUP BY pl.productline
 ORDER BY SUM(pr.quantityinstock) DESC*/
 
-# OJO SIN NECESIDAD DE JOIN!!!
+)
 
-/*SELECT productline AS ProductLine, SUM(quantityinstock) AS Stock, 
-ROUND((SUM(quantityinstock) / (SELECT SUM(quantityinstock) FROM products)) * 100,1) AS StockPercentage
-FROM products
-GROUP BY productline
-ORDER BY SUM(quantityinstock) DESC*/
 
 #VER LA STOCK TOTAL PARA LUEGO SACAR CONCLUSIONES
 
